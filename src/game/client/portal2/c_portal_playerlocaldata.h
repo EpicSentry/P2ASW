08fe7c6011e0f4abf40312e99e8b964668b9bad5
Mandrew <Wonderland_War@outlook.com>
2023-09-22 15:33:10 -0500

    More requirements

---
+++ src/game/client/portal2/c_portal_playerlocaldata.h
@@ -0,0 +1,72 @@
+#ifndef PORTAL_PLAYER_LOCAL_DATA_H
+#define PORTAL_PLAYER_LOCAL_DATA_H
+
+#include "cbase.h"
+#include "portal_player_shared.h"
+
+#define CPortalPlayerLocalData C_PortalPlayerLocalData
+
+class C_PortalPlayerLocalData
+{
+public:
+	
+	DECLARE_CLASS_NOBASE( C_PortalPlayerLocalData );
+	DECLARE_EMBEDDED_NETWORKVAR();
+	DECLARE_SIMPLE_DATADESC();
+
+	StickCameraState m_nStickCameraState;
+	InAirState m_InAirState;
+	PaintPowerType m_PaintedPowerType;
+
+	int m_nLocatorEntityIndices;
+
+	bool m_bShowingViewFinder;
+	bool m_bPlacingPhoto;
+	bool m_bDoneStickInterp;
+	bool m_bDoneCorrectPitch;
+	bool m_bAttemptHullResize;
+	bool m_bJumpedThisFrame;
+	bool m_bBouncedThisFrame;
+	bool m_bDuckedInAir;
+	bool m_bZoomedIn;
+	bool m_bPreventedCrouchJumpThisFrame;
+
+	float m_flAirControlSupressionTime;
+	float m_flAirInputScale;
+	float m_flCurrentStickTime;
+	float m_fBouncedTime;
+
+	CountdownTimer m_PaintedPowerTimer;
+	CachedPaintPowerChoiceResult m_CachedPaintPowerChoiceResults[PAINT_POWER_TYPE_COUNT];
+
+	Vector m_StickNormal;
+	Vector m_OldStickNormal;
+	Vector m_vPreUpdateVelocity;
+	Vector m_Up;
+	Vector m_vStickRotationAxis;
+	Vector m_StandHullMin;
+	Vector m_StandHullMax;
+	Vector m_DuckHullMin;
+	Vector m_DuckHullMax;
+	Vector m_CachedStandHullMinAttempt;
+	Vector m_CachedStandHullMaxAttempt;
+	Vector m_CachedDuckHullMinAttempt;
+	Vector m_CachedDuckHullMaxAttempt;
+	Vector m_vLocalUp;
+	Vector m_vEyeOffset;
+
+	QAngle m_qQuaternionPunch;
+
+	EHANDLE m_hTractorBeam; // Was probably CHandle<> instead
+
+	// Client only
+
+	Vector m_PaintPowerHudInfoPosition[PAINT_POWER_TYPE_COUNT];
+	Vector m_PaintPowerHudInfoNormal[PAINT_POWER_TYPE_COUNT];
+
+	// Hard to tell what type this was.
+	bool m_PaintPowerHudInfoValidity[PAINT_POWER_TYPE_COUNT];
+
+
+};
+#endif //PORTAL_PLAYER_LOCAL_DATA_H
