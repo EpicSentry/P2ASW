471dd09102c1130b4aafdd2067504e64b895f9c3
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2023-09-24 20:57:44 +1300

    Reformatted the npc_personality_core code

--- src/game/server/portal2/npc_personality_core.cpp
+++ src/game/server/portal2/npc_personality_core.cpp
@@ -9,25 +9,50 @@
 #include "choreoscene.h"
 #include "choreoactor.h"
 
+ConVar sv_personality_core_pca_pitch( "sv_personality_core_pca_pitch", "180", 0, "Pitch value for personality core perferred carry angles." );
+ConVar sv_personality_core_pca_yaw( "sv_personality_core_pca_yaw", "-90", 0, "Yaw value for personality core perferred carry angles." );
+ConVar sv_personality_core_pca_roll( "sv_personality_core_pca_roll", "195", 0, "Roll value for personality core perferred carry angles." );
+
 #define	CORE_MODEL "models/npcs/personality_sphere/personality_sphere.mdl"
 #define	CORE_SKINS_MODEL "models/npcs/personality_sphere/personality_sphere_skins.mdl"
 
-class CNPCPersonalityCore : public CAI_PlayerAlly, public CDefaultPlayerPickupVPhysics
+class CNPC_PersonalityCore : public CAI_PlayerAlly, public CDefaultPlayerPickupVPhysics
 {
 public:
-	DECLARE_CLASS(CNPCPersonalityCore, CAI_PlayerAlly);
+	DECLARE_CLASS(CNPC_PersonalityCore, CAI_PlayerAlly);
 	DECLARE_DATADESC();
 
-	CNPCPersonalityCore()
+	CNPC_PersonalityCore()
 	{
 	}
+
+	void Precache(void);
+	void Spawn(void);
+
+	void InputEnableMotion(inputdata_t& inputdata);
+	void InputDisableMotion(inputdata_t& inputdata);
+	void InputEnablePickup(inputdata_t& inputdata);
+	void InputDisablePickup(inputdata_t& inputdata);
+	void InputForcePickup(inputdata_t& inputdata);
+	void InputPlayLock(inputdata_t& inputdata);
+	void InputPlayAttach(inputdata_t& inputdata);
+	void InputPlayDetach(inputdata_t& inputdata);
+	void InputSetIdleSequence(inputdata_t& inputdata);
+	void InputClearIdleSequence(inputdata_t& inputdata);
+	void InputClearParent(inputdata_t& inputdata);
+
+	int	TranslateSchedule(int scheduleType);
+	void GatherConditions();
+	void OnPhysGunPickup(CBasePlayer* pPhysGunUser, PhysGunPickup_t reason);
+	void OnPhysGunDrop(CBasePlayer* pPhysGunUser, PhysGunDrop_t reason);
+	void OnFizzled();
 
 	bool CreateVPhysics(void)
 	{
 		VPhysicsDestroyObject();
 		RemoveSolidFlags(FSOLID_NOT_SOLID);
 		SetSolid(SOLID_BBOX);
-		IPhysicsObject *pPhysicsObject = VPhysicsInitNormal(SOLID_VPHYSICS, GetSolidFlags(), false);
+		IPhysicsObject* pPhysicsObject = VPhysicsInitNormal(SOLID_VPHYSICS, GetSolidFlags(), false);
 		if (pPhysicsObject == NULL)
 			return false;
 
@@ -35,69 +60,51 @@ public:
 		SetMoveType(MOVETYPE_VPHYSICS, MOVECOLLIDE_DEFAULT);
 		return true;
 	}
-
-	void Spawn(void);
-	void Precache(void);
-
-	void Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value);
+	
 	int ObjectCaps();
-
-	void OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason);
-	void OnPhysGunDrop(CBasePlayer *pPhysGunUser, PhysGunDrop_t reason);
-
+	void Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value);
+	QAngle PreferredCarryAngles();
+	bool HasPreferredCarryAnglesForPlayer(CBasePlayer* pPlayer) { return true; }
+	CAI_Expresser* CreateExpresser();
 	bool IsOkToSpeakInResponseToPlayer(void);
-	bool TestRemarkingUpon(CInfoRemarkable* pRemarkable);
-	void OnFizzled();
-	void HandleAnimEvent(animevent_t* pEvent);
-	void PrescheduleThink(void);
-	int	TranslateSchedule(int scheduleType);
-
-	void InputEnablePickup(inputdata_t& inputdata);
-	void InputDisablePickup(inputdata_t& inputdata);
-	void InputForcePickup(inputdata_t& inputdata);
-	void InputSetIdleSequence(inputdata_t& inputdata);
-	void InputPlayAttach(inputdata_t& inputdata);
-	void InputPlayDetach(inputdata_t& inputdata);
-	void InputPlayLock(inputdata_t& inputdata);
-	void InputEnableMotion(inputdata_t& inputdata);
-	void InputDisableMotion(inputdata_t& inputdata);
 	bool StartSceneEvent(CSceneEventInfo* info, CChoreoScene* scene, CChoreoEvent* event, CChoreoActor* actor, CBaseEntity* pTarget);
-	void GatherConditions();
-
-	bool m_bUseAltModel;
-	bool m_bPickupEnabled;
-	int m_iIdleOverrideSequence;
-	float m_flAnimResetTime;
-	bool m_bAttached;
-	int m_nIdealSequence;
+	void PrescheduleThink(void);
+protected:
+	bool TestRemarkingUpon(CInfoRemarkable* pRemarkable);
 	bool m_bHasBeenPickedUp;
-
-private:
-	CHandle<CBasePlayer>	m_hPhysicsAttacker;
+	bool m_bPickupEnabled;
+	bool m_bAttached;
+	bool m_bUseAltModel;
 	COutputEvent m_OnPlayerPickup;
-	COutputEvent m_OnPhysGunDrop;
+	COutputEvent m_OnPlayerDrop;
+	float m_flAnimResetTime;
+	int m_iIdleOverrideSequence;
+private:
+	CAI_Expresser* m_pExpresser;
 };
 
-LINK_ENTITY_TO_CLASS(npc_personality_core, CNPCPersonalityCore);
+LINK_ENTITY_TO_CLASS(npc_personality_core, CNPC_PersonalityCore);
 
-BEGIN_DATADESC(CNPCPersonalityCore)
-DEFINE_KEYFIELD(m_bUseAltModel, FIELD_BOOLEAN, "altmodel"),
+BEGIN_DATADESC(CNPC_PersonalityCore)
+DEFINE_INPUTFUNC(FIELD_VOID, "EnableMotion", InputEnableMotion),
+DEFINE_INPUTFUNC(FIELD_VOID, "DisableMotion", InputDisableMotion),
 DEFINE_INPUTFUNC(FIELD_VOID, "EnablePickup", InputEnablePickup),
 DEFINE_INPUTFUNC(FIELD_VOID, "DisablePickup", InputDisablePickup),
 DEFINE_INPUTFUNC(FIELD_VOID, "ForcePickup", InputForcePickup),
-DEFINE_INPUTFUNC(FIELD_VOID, "SetIdleSequence", InputSetIdleSequence),
+DEFINE_INPUTFUNC(FIELD_VOID, "PlayLock", InputPlayLock),
 DEFINE_INPUTFUNC(FIELD_VOID, "PlayAttach", InputPlayAttach),
 DEFINE_INPUTFUNC(FIELD_VOID, "PlayDetach", InputPlayDetach),
-DEFINE_INPUTFUNC(FIELD_VOID, "PlayLock", InputPlayLock),
-DEFINE_INPUTFUNC(FIELD_VOID, "EnableMotion", InputEnableMotion),
-DEFINE_INPUTFUNC(FIELD_VOID, "DisableMotion", InputDisableMotion),
+DEFINE_INPUTFUNC(FIELD_VOID, "SetIdleSequence", InputSetIdleSequence),
+DEFINE_INPUTFUNC(FIELD_VOID, "ClearIdleSequence", InputClearIdleSequence),
+DEFINE_INPUTFUNC(FIELD_VOID, "ClearParent", InputClearParent),
 DEFINE_USEFUNC(Use),
+DEFINE_KEYFIELD(m_bUseAltModel, FIELD_BOOLEAN, "altmodel"),
 DEFINE_OUTPUT(m_OnPlayerPickup, "OnPlayerPickup"),
-DEFINE_OUTPUT(m_OnPhysGunDrop, "OnPhysGunDrop"),
+DEFINE_OUTPUT(m_OnPlayerDrop, "OnPlayerDrop"),
 
 END_DATADESC()
 
-void CNPCPersonalityCore::Precache(void)
+void CNPC_PersonalityCore::Precache(void)
 {
 	if (m_bUseAltModel == false) {
 		PrecacheModel(CORE_MODEL);
@@ -110,8 +117,11 @@ void CNPCPersonalityCore::Precache(void)
 	BaseClass::Precache();
 }
 
-void CNPCPersonalityCore::Spawn(void)
+void CNPC_PersonalityCore::Spawn(void)
 {
+	Vector vecAttachOrigin;
+	Vector vecAttachAngles;
+
 	Precache();
 	CapabilitiesClear();
 	if (m_bUseAltModel == false) {
@@ -145,11 +155,10 @@ void CNPCPersonalityCore::Spawn(void)
 	m_bAttached = false;
 	m_iIdleOverrideSequence = -1;
 
-	if (m_bUseAltModel == false)
+	if ( !m_bUseAltModel )
 	{
-		int iEyes = LookupAttachment("eyes");
-		matrix3x4_t eyesToWorld;
-		GetAttachment(iEyes, eyesToWorld);
+		int iAttachmentIndex = LookupAttachment("eyes");
+		GetAttachment(iAttachmentIndex, vecAttachOrigin, &vecAttachAngles);
 
 		CBaseEntity* pProjectedTexture = CreateEntityByName("env_projectedtexture");
 		pProjectedTexture->KeyValue("lightfov", "85.0f");
@@ -162,191 +171,221 @@ void CNPCPersonalityCore::Spawn(void)
 	if (m_pParent != NULL)
 		AddSolidFlags(FSOLID_NOT_SOLID);
 
-	return;
 }
 
-int CNPCPersonalityCore::ObjectCaps()
-{
-	return UsableNPCObjectCaps(BaseClass::ObjectCaps());
-}
-
-void CNPCPersonalityCore::Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value)
-{
-	CBasePlayer* pPlayer = ToBasePlayer(pActivator);
-
-	if (((m_bPickupEnabled != false) && (pActivator != false)) && (pPlayer != false)) {
-		SetParent(0,-1);
-		CreateVPhysics();
-		pPlayer->PickupObject(this, true);
-	}
-	return;
-}
-
-void CNPCPersonalityCore::OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason)
-{
-	m_hPhysicsAttacker = pPhysGunUser;
-
-	if (reason == PICKED_UP_BY_CANNON || reason == PICKED_UP_BY_PLAYER)
-	{
-		m_OnPlayerPickup.FireOutput(pPhysGunUser, this);
-	}
-}
-
-void CNPCPersonalityCore::OnPhysGunDrop(CBasePlayer *pPhysGunUser, PhysGunDrop_t reason)
-{
-	m_OnPhysGunDrop.FireOutput(pPhysGunUser, this);
-}
-
-void CNPCPersonalityCore::InputEnablePickup(inputdata_t& inputdata)
-{
-	m_bPickupEnabled = true;
-}
-
-void CNPCPersonalityCore::InputDisablePickup(inputdata_t& inputdata)
-{
-	m_bPickupEnabled = false;
-}
-
-void CNPCPersonalityCore::InputForcePickup(inputdata_t& inputdata)
-{
-	CBasePlayer* pPlayer = UTIL_GetLocalPlayer();
-
-	if (pPlayer != false) {
-		SetParent(0, -1);
-		CreateVPhysics();
-		pPlayer->PickupObject(this, true);
-	}
-}
-
-void CNPCPersonalityCore::InputSetIdleSequence(inputdata_t& inputdata)
-{
-	m_iIdleOverrideSequence = LookupSequence(STRING(inputdata.value.StringID()));
-	m_flAnimResetTime = -1.0f;
-}
-
-bool CNPCPersonalityCore::IsOkToSpeakInResponseToPlayer(void)
-{
-	return IsOkToSpeak(SPEECH_PRIORITY,true);
-}
-
-bool CNPCPersonalityCore::TestRemarkingUpon(CInfoRemarkable* pRemarkable)
-{
-	return IsLineOfSightClear( pRemarkable, IGNORE_ACTORS );
-}
-
-void CNPCPersonalityCore::OnFizzled()
-{
-	ResponseRules::CRR_Concept::CRR_Concept("TLK_FIZZLED");
-	BaseClass::OnFizzled();
-}
-
-void CNPCPersonalityCore::HandleAnimEvent(animevent_t* pEvent)
-{
-	if (m_pPrimaryBehavior != false) {
-		m_pPrimaryBehavior->HandleAnimEvent(pEvent);
-	return;
-	}
-	BaseClass::HandleAnimEvent(pEvent);
-	return;
-}
-
-void CNPCPersonalityCore::PrescheduleThink()
-{
-	{
-	if ((m_flAnimResetTime < (gpGlobals->curtime) && (m_flAnimResetTime != 0.0f))) {
-		if (m_iIdleOverrideSequence < 0) {
-			if (m_bAttached == false) {
-				SetIdealActivity(ACT_IDLE);
-			}
-			else {
-				m_nIdealSequence = LookupSequence("sphere_plug_idle_neutral");
-				SetIdealActivity(ACT_SPECIFIC_SEQUENCE);
-			}
-		}
-		else {
-			SetIdealActivity(ACT_SPECIFIC_SEQUENCE);
-		}
-		m_flCycle = 0.0f;
-	}
-	m_flAnimResetTime = 0.0f;
-	}
-	BaseClass::PrescheduleThink();
-	return;
-}
-
-void CNPCPersonalityCore::InputPlayAttach(inputdata_t& inputdata)
-{
-	InstancedScriptedScene(this,"scenes/npc/sp_proto_sphere/sphere_plug_attach.vcd");
-	m_bAttached = true;
-	return;
-}
-
-void CNPCPersonalityCore::InputPlayDetach(inputdata_t& inputdata)
-{
-	m_bAttached = false;
-	m_flAnimResetTime = -1.0f;
-	RemoveSolidFlags(FSOLID_NOT_SOLID);
-	return;
-}
-
-void CNPCPersonalityCore::InputPlayLock(inputdata_t& inputdata)
-{
-	InstancedScriptedScene(this, "scenes/npc/sp_proto_sphere/sphere_plug_lock.vcd");
-	SetSolidFlags(FSOLID_NOT_SOLID);
-}
-
-int CNPCPersonalityCore::TranslateSchedule(int schedule)
-{
-	return BaseClass::TranslateSchedule( schedule );
-}
-
-void CNPCPersonalityCore::InputEnableMotion(inputdata_t& inputdata)
+void CNPC_PersonalityCore::InputEnableMotion(inputdata_t& inputdata)
 {
 	IPhysicsObject* pPhysicsObject = VPhysicsGetObject();
-	if (pPhysicsObject != NULL ) {
+
+	if (pPhysicsObject)
+	{
 		pPhysicsObject->EnableMotion(true);
 		pPhysicsObject->Wake();
 	}
 	CreateVPhysics();
-	return;
 }
 
-void CNPCPersonalityCore::InputDisableMotion(inputdata_t& inputdata)
+void CNPC_PersonalityCore::InputDisableMotion(inputdata_t& inputdata)
 {
 	IPhysicsObject* pPhysicsObject = VPhysicsGetObject();
 	if (pPhysicsObject != NULL) {
 		pPhysicsObject->EnableMotion(false);
 	}
-	return;
 }
 
-bool CNPCPersonalityCore::StartSceneEvent( CSceneEventInfo *info, CChoreoScene *scene, CChoreoEvent *event, CChoreoActor *actor, CBaseEntity *pTarget )
+void CNPC_PersonalityCore::InputEnablePickup(inputdata_t& inputdata)
 {
-	switch (info->m_pEvent->GetType() )
-	case CChoreoEvent::SEQUENCE: 
-	{
-		return BaseClass::StartSceneEvent(info, scene, event, actor, pTarget);
-	}
-
-	info->m_nSequence = LookupSequence(event->GetParameters());
-
-	if (info->m_nSequence < 0)
-		return false;
-
-	ResetIdealActivity(ACT_SPECIFIC_SEQUENCE);
-	m_nIdealSequence = m_nSequence;
-	float flResetTime = m_flAnimResetTime;
-
-	float flDuration = (event->GetEndTime() - scene->GetTime());
-	if ((flDuration) + (gpGlobals->curtime < flResetTime)) {
-		return true;
-	}
-
-	m_flAnimResetTime = (flDuration) + (gpGlobals->curtime);
-	return true;
+	m_bPickupEnabled = true;
 }
 
-void CNPCPersonalityCore::GatherConditions()
+void CNPC_PersonalityCore::InputDisablePickup(inputdata_t& inputdata)
+{
+	m_bPickupEnabled = false;
+}
+
+void CNPC_PersonalityCore::InputForcePickup(inputdata_t& inputdata)
+{
+	CBasePlayer* LocalPlayer = UTIL_GetLocalPlayer();
+
+	if (LocalPlayer)
+	{
+		SetParent(0, -1);
+		CreateVPhysics();
+		LocalPlayer->PickupObject(this, true);
+	}
+}
+
+void CNPC_PersonalityCore::InputPlayLock(inputdata_t& inputdata)
+{
+	InstancedScriptedScene(this, "scenes/npc/sp_proto_sphere/sphere_plug_lock.vcd");
+	SetSolidFlags(FSOLID_NOT_SOLID);
+}
+
+void CNPC_PersonalityCore::InputPlayAttach(inputdata_t& inputdata)
+{
+	InstancedScriptedScene(this, "scenes/npc/sp_proto_sphere/sphere_plug_attach.vcd");
+	m_bAttached = true;
+}
+
+void CNPC_PersonalityCore::InputPlayDetach(inputdata_t& inputdata)
+{
+	m_bAttached = false;
+	m_flAnimResetTime = -1.0f;
+	RemoveSolidFlags(FSOLID_NOT_SOLID);
+}
+
+void CNPC_PersonalityCore::InputSetIdleSequence(inputdata_t& inputdata)
+{
+	m_iIdleOverrideSequence = LookupSequence(STRING(inputdata.value.StringID()));
+	m_flAnimResetTime = -1.0f;
+}
+
+void CNPC_PersonalityCore::InputClearIdleSequence(inputdata_t& inputdata)
+{
+	m_iIdleOverrideSequence = -1;
+	m_flAnimResetTime = -1.0f;
+}
+
+void CNPC_PersonalityCore::InputClearParent(inputdata_t& inputdata)
+{
+	BaseClass::InputClearParent(inputdata);
+	RemoveSolidFlags(FSOLID_NOT_SOLID);
+}
+
+int CNPC_PersonalityCore::TranslateSchedule(int schedule)
+{
+	return BaseClass::TranslateSchedule(schedule);
+}
+
+void CNPC_PersonalityCore::GatherConditions()
 {
 	BaseClass::GatherConditions();
 }
+
+void CNPC_PersonalityCore::OnPhysGunPickup(CBasePlayer* pPhysGunUser, PhysGunPickup_t reason)
+{
+	if (reason == PICKED_UP_BY_PLAYER)
+	{
+		m_OnPlayerPickup.FireOutput(pPhysGunUser, this);
+	}
+}
+
+void CNPC_PersonalityCore::OnPhysGunDrop(CBasePlayer* pPhysGunUser, PhysGunDrop_t reason)
+{
+	m_OnPlayerDrop.FireOutput(pPhysGunUser, this);
+}
+
+void CNPC_PersonalityCore::OnFizzled()
+{
+	ResponseRules::CRR_Concept::CRR_Concept("TLK_FIZZLED");
+	BaseClass::OnFizzled();
+}
+
+int CNPC_PersonalityCore::ObjectCaps()
+{
+	return UsableNPCObjectCaps(BaseClass::ObjectCaps());
+}
+
+void CNPC_PersonalityCore::Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value)
+{
+	CBasePlayer* pPlayer = ToBasePlayer(pActivator);
+
+	if (m_bPickupEnabled && pActivator && pPlayer )
+	{
+		SetParent(0,-1);
+		CreateVPhysics();
+		pPlayer->PickupObject(this, true);
+	}
+}
+
+QAngle CNPC_PersonalityCore::PreferredCarryAngles()
+{
+	return QAngle(sv_personality_core_pca_pitch.GetMaxValue(), sv_personality_core_pca_yaw.GetMaxValue(), sv_personality_core_pca_roll.GetMaxValue());
+}
+
+CAI_Expresser* CNPC_PersonalityCore::CreateExpresser()
+{
+	m_pExpresser = new CAI_ExpresserWithFollowup(this);
+	if (!m_pExpresser)
+		return NULL;
+
+	m_pExpresser->Connect(this);
+	return m_pExpresser;
+}
+
+bool CNPC_PersonalityCore::IsOkToSpeakInResponseToPlayer(void)
+{
+	return IsOkToSpeak(SPEECH_PRIORITY,true);
+}
+
+bool CNPC_PersonalityCore::StartSceneEvent(CSceneEventInfo* info, CChoreoScene* scene, CChoreoEvent* event, CChoreoActor* actor, CBaseEntity* pTarget)
+{
+	bool result;
+	double GetTime;
+	float infoa;
+	float infob;
+	float infoc;
+	float eventa;
+
+	if (event->GetType() != CChoreoEvent::SEQUENCE)
+		return BaseClass::StartSceneEvent(info, scene, event, actor, pTarget);
+
+	const char* Parameters = event->GetParameters();
+	int ISequence = LookupSequence(Parameters);
+
+	if (ISequence < 0)
+		return 0;
+
+	ResetIdealActivity(ACT_SPECIFIC_SEQUENCE);
+	infoa = m_flAnimResetTime;
+	SetIdealSequence(ISequence);
+	eventa = event->GetEndTime();
+	if (infoa <= eventa - scene->GetTime() + gpGlobals->curtime)
+	{
+		infob = event->GetEndTime();
+		GetTime = scene->GetTime();
+		result = true;
+		infoc = infob - GetTime + gpGlobals->curtime;
+		m_flAnimResetTime = infoc;
+	}
+	else
+	{
+		this->m_flAnimResetTime = infoa;
+		return 1;
+	}
+	return result;
+}
+
+void CNPC_PersonalityCore::PrescheduleThink()
+{
+	float m_flAnimResetTime = this->m_flAnimResetTime;
+	if (gpGlobals->curtime > m_flAnimResetTime && m_flAnimResetTime != 0.0f)
+	{
+		m_iIdleOverrideSequence = this->m_iIdleOverrideSequence;
+		if (m_iIdleOverrideSequence <= -1)
+		{
+			if (m_bAttached)
+			{
+				int iPlugidle = LookupSequence("sphere_plug_idle_neutral");
+				SetIdealActivity(ACT_SPECIFIC_SEQUENCE);
+				SetIdealSequence(iPlugidle);
+			}
+			else
+			{
+				SetIdealActivity(ACT_IDLE);
+			}
+		}
+		else
+		{
+			SetIdealActivity(ACT_SPECIFIC_SEQUENCE);
+		}
+		m_flAnimResetTime = 0.0f;
+	}
+	BaseClass::PrescheduleThink();
+}
+
+bool CNPC_PersonalityCore::TestRemarkingUpon(CInfoRemarkable* pRemarkable)
+{
+	return IsLineOfSightClear( pRemarkable, IGNORE_ACTORS );
+}
+
