3688233c6e2f538089b0137ad5021fce74acf280
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2023-09-26 17:11:28 +1300

    Add files via upload

---
+++ src/game/client/portal2/c_npc_wheatley_boss.cpp
@@ -0,0 +1,14 @@
+#include "cbase.h";
+#include <c_ai_basenpc.h>
+
+class C_NPC_Wheatley_Boss : public C_AI_BaseNPC
+{
+public:
+	DECLARE_CLASS(C_NPC_Wheatley_Boss, C_AI_BaseNPC);
+	DECLARE_CLIENTCLASS();
+};
+
+IMPLEMENT_CLIENTCLASS_DT(C_NPC_Wheatley_Boss, DT_NPC_Wheatley_Boss, CNPC_Wheatley_Boss)
+END_RECV_TABLE()
+
+LINK_ENTITY_TO_CLASS(npc_wheatley_boss, C_NPC_Wheatley_Boss);
