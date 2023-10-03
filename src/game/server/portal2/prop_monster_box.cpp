43adeabd6a8d38f8f2edf90c100c625b3511cab3
Mandrew <Wonderland_War@outlook.com>
2023-10-02 19:38:44 -0500

    Implement monster boxes

--- src/game/server/portal2/prop_monster_box.cpp
+++ src/game/server/portal2/prop_monster_box.cpp
@@ -1,161 +1,485 @@
 #include "cbase.h"
-#include "ai_basenpc.h"
-#include "baseanimating.h"
-#include "sprite.h"
-#include "props.h"
-#include "particle_parse.h"
-#include "particle_system.h"
-#include "player_pickup.h"
+#include "prop_monster_box.h"
+#include "portal_grabcontroller_shared.h"
+#include "trigger_portal_cleanser.h"
 
-#define	CUBE_MODEL	"models/npcs/monsters/monster_a.mdl"
+const char *s_pAnimateThinkContext = "AnimateThinkContext";
 
-class CPropMonsterBox : public CDynamicProp
-{
-public:
-	DECLARE_CLASS(CPropMonsterBox, CDynamicProp);
-	DECLARE_DATADESC();
 
-	CPropMonsterBox()
-	{
-	}
+ConVar sv_monster_turret_velocity( "sv_monster_turret_velocity", "100.0f", FCVAR_REPLICATED, "The amount of velocity the monster turret tries to move with." );
 
-	bool CreateVPhysics()
-	{
-		VPhysicsInitNormal(SOLID_VPHYSICS, 0, false);
-		return true;
-	}
 
-	void JumpThink(void);
-	void Spawn(void);
-	void Precache(void);
-	void OnUse();
+BEGIN_DATADESC( CPropMonsterBox )
 
-	//Use
-	void Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value);
-	int ObjectCaps();
+	DEFINE_KEYFIELD( m_bIsABox, FIELD_BOOLEAN, "StartAsBox" ),
+	DEFINE_KEYFIELD( m_bAllowSilentDissolve, FIELD_BOOLEAN, "AllowSilentDissolve" ),
+	DEFINE_KEYFIELD( m_flBoxSwitchSpeed, FIELD_FLOAT, "BoxSwitchSpeed" ),
 
-	void ReachedEndOfSequence(void);
+	DEFINE_INPUTFUNC( FIELD_VOID, "BecomeBox", InputBecomeBox ),
+	DEFINE_INPUTFUNC( FIELD_VOID, "BecomeMonster", InputBecomeMonster ),
+	DEFINE_INPUTFUNC( FIELD_VOID, "BecomeShortcircuit", InputBecomeShortcircuit ),
+	DEFINE_INPUTFUNC( FIELD_VOID, "Dissolve", InputDissolve ),
+	DEFINE_INPUTFUNC( FIELD_VOID, "SilentDissolve", InputSilentDissolve ),
 
-	//Pickup
-	void Pickup(void);
-	void OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason);
-	void OnPhysGunDrop(CBasePlayer *pPhysGunUser, PhysGunDrop_t reason);
-
-private:
-	CHandle<CBasePlayer>	m_hPhysicsAttacker;
-
-	COutputEvent m_OnPlayerPickup;
-	COutputEvent m_OnPhysGunDrop;
-	COutputEvent m_OnExplode;
-};
-
-LINK_ENTITY_TO_CLASS(prop_monster_box, CPropMonsterBox);
-
-BEGIN_DATADESC(CPropMonsterBox)
-// Save/load
-DEFINE_USEFUNC(Use),
-// Output
-DEFINE_OUTPUT(m_OnPlayerPickup, "OnPlayerPickup"),
-DEFINE_OUTPUT(m_OnPhysGunDrop, "OnPhysGunDrop"),
+DEFINE_FIELD( m_bIsShortcircuit, FIELD_BOOLEAN ),
 
 END_DATADESC()
 
-void CPropMonsterBox::Precache(void)
+LINK_ENTITY_TO_CLASS( prop_monster_box, CPropMonsterBox )
+
+CPropMonsterBox::CPropMonsterBox()
 {
-	PrecacheModel(CUBE_MODEL);
-	BaseClass::Precache();
+
 }
 
-void CPropMonsterBox::Spawn(void)
+CPropMonsterBox::~CPropMonsterBox()
 {
+
+}
+
+void CPropMonsterBox::Spawn( void )
+{
+	const char *pszModelName = "models/npcs/monsters/monster_A_box.mdl";
+	if (!m_bForcedAsBox)
+		pszModelName = "models/npcs/monsters/monster_a.mdl";
+	
+	SetModel(pszModelName);
+	
 	Precache();
+
+	CollisionProp()->SetSolid( SOLID_BBOX );
+	AddEffects( 1024 );
+	//g_PortalGameStats.Event_CubeSpawn();
 	BaseClass::Spawn();
-
-	SetModel(CUBE_MODEL);
-	SetSolid(SOLID_VPHYSICS);
-
-	// In order to pick it up, needs to be physics.
-	CreateVPhysics();
-
-	SetUse(&CPropMonsterBox::Use);
-
-	ReachedEndOfSequence();
-}
-
-int CPropMonsterBox::ObjectCaps()
-{
-	int caps = BaseClass::ObjectCaps();
-
-	caps |= FCAP_IMPULSE_USE;
-
-	return caps;
-}
-
-void CPropMonsterBox::Use(CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value)
-{
-	CBasePlayer *pPlayer = ToBasePlayer(pActivator);
-	if (pPlayer)
+	if ( m_bForcedAsBox )
 	{
-		pPlayer->PickupObject(this);
-		OnUse();
+		ResetSequence( LookupSequence("hermit_idle") );
+
+		m_bIsABox = true;
 	}
-}
-
-void CPropMonsterBox::OnUse()
-{
-}
-
-void CPropMonsterBox::OnPhysGunPickup(CBasePlayer *pPhysGunUser, PhysGunPickup_t reason)
-{
-	m_hPhysicsAttacker = pPhysGunUser;
-
-	if (reason == PICKED_UP_BY_CANNON || reason == PICKED_UP_BY_PLAYER)
+	else
 	{
-		m_OnPlayerPickup.FireOutput(pPhysGunUser, this);
+		ResetSequence( LookupSequence("straight01") );
+		
+		m_bIsABox = false;
 	}
+
+	m_nBodyGroups = RandomInt( 0, 15);
+	m_nBodyGroups |= RandomInt( 0, 15);
+	m_nBodyGroups |= RandomInt( 0, 15);
+	
+
+	SetBodygroup(1, m_nBodyGroups & 1);
+	SetBodygroup(2, (m_nBodyGroups >> 1) & 1);
+	SetBodygroup(3, (m_nBodyGroups >> 2) & 1);
+	SetBodygroup(4, (m_nBodyGroups >> 3) & 1);
+	
+	SetMoveType( MOVETYPE_VPHYSICS, MOVECOLLIDE_DEFAULT );
+
+
+	SetContextThink( &CPropMonsterBox::AnimateThink, gpGlobals->curtime + 0.1, s_pAnimateThinkContext );
+
+	if (m_pPhysicsObject)
+		m_pPhysicsObject->SetMass(40.0);
+	
+	UpdateObjectCapsCache();
+	SetFadeDistance(-1.0, 0.0);
+	SetGlobalFadeScale(0.0);
 }
 
-void CPropMonsterBox::OnPhysGunDrop(CBasePlayer *pPhysGunUser, PhysGunDrop_t reason)
+void CPropMonsterBox::Precache( void )
 {
-	m_OnPhysGunDrop.FireOutput(pPhysGunUser, this);
+	BaseClass::Precache();
+	PrecacheModel("models/npcs/monsters/monster_a.mdl");
+	PrecacheModel("models/npcs/monsters/monster_A_box.mdl");
+	PrecacheScriptSound("DoSparkSmaller");
 }
 
-void CPropMonsterBox::ReachedEndOfSequence(void)
+void CPropMonsterBox::BecomeBox( bool bForce )
 {
-	/*Vector vecStartPos = GetAbsOrigin();
+	if ( m_bIsShortcircuit )
+		return;
 
-	Vector vecEndPos = GetAbsOrigin();
-	vecEndPos.z -= 16;
-
-	trace_t	belowTrace;
-	UTIL_TraceLine(vecStartPos, vecEndPos, MASK_ALL, this, COLLISION_GROUP_PLAYER, &belowTrace);
-
-	if (belowTrace.DidHit())
+	//m_bForcedAsBox |= bForce;
+	m_bForcedAsBox = bForce;
+	if (m_bIsABox)
 	{
-		Vector vecStartPos = GetAbsOrigin();
-
-		Vector vecEndPos = GetAbsOrigin();
-		vecEndPos.y += 16;
-
-		trace_t	frontTrace;
-		UTIL_TraceLine(vecStartPos, vecEndPos, MASK_ALL, this, COLLISION_GROUP_PLAYER, &frontTrace);
-
-		if (frontTrace.DidHit())
+		m_nDeferredTransform = 0;
+	}
+	else if (m_bHeld)
+	{
+		m_nDeferredTransform = 1;
+	}
+	else
+	{
+		m_bIsABox = true;
+		if (m_pPhysicsObject)
 		{
-			PropSetAnim("trapped");
+			Vector vecVelocity;
+			Vector vecAngVelocity;
+
+			m_pPhysicsObject->GetVelocity( &vecVelocity, &vecAngVelocity );
+			SetModel("models/npcs/monsters/monster_A_box.mdl");
+			VPhysicsDestroyObject();
+			CreateVPhysics();
+
+			m_pPhysicsObject->SetVelocity( &vecVelocity, &vecAngVelocity );
+				
+			ResetSequence( LookupSequence("hermit_in") );
+		}
+	}
+}
+
+void CPropMonsterBox::BecomeMonster( bool bForce )
+{
+	if ( m_bIsShortcircuit )
+		return;
+
+	Vector vecVelocity; // [esp+4h] [ebp-18h] BYREF
+	Vector vecAngVelocity; // [esp+10h] [ebp-Ch] BYREF
+
+	if ( bForce )
+	{
+		m_bForcedAsBox = false;
+	}
+	else if (m_bForcedAsBox)
+	{
+		return;
+	}
+	if (m_bIsABox)
+	{
+		if (m_bHeld)
+		{
+			m_nDeferredTransform = 2;
 		}
 		else
 		{
-		*/
-			int sequenceToPerform = RandomInt(1, 3);
-			char* seqName = new char[32];
-			Q_snprintf(seqName, 32, "straight0%d", sequenceToPerform);
-			PropSetAnim(seqName);
-			/*
+			m_bIsABox = false;
+			if (m_pPhysicsObject)
+			{
+				m_pPhysicsObject->GetVelocity( &vecVelocity, &vecAngVelocity );
+				
+				SetModel("models/npcs/monsters/monster_a.mdl");
+				VPhysicsDestroyObject();
+				CreateVPhysics();
+
+				m_pPhysicsObject->SetVelocity( &vecVelocity, &vecAngVelocity );
+				
+				CBaseAnimating::ResetSequence( LookupSequence("hermit_out") );
+			}
 		}
 	}
 	else
 	{
-		PropSetAnim("intheair");
-	}*/
+		m_nDeferredTransform = 0;
+	}
+}
+
+void CPropMonsterBox::BecomeShortcircuit( void )
+{
+	if (m_pPhysicsObject)
+	{
+		if (!m_bIsABox)
+		{
+			Vector vecVelocity;
+			Vector vecAngVelocity;
+			m_pPhysicsObject->GetVelocity( &vecVelocity, &vecAngVelocity );
+			SetModel("models/npcs/monsters/monster_A_box.mdl");
+			VPhysicsDestroyObject();
+			CreateVPhysics();
+			
+			m_pPhysicsObject->SetVelocity( &vecVelocity, &vecAngVelocity );
+		}
+
+		// NOTE: This was UTIL_GetLocalPlayer, but this won't work well in multiplayer and will force the player
+		// to drop what they're grabbing even if they aren't holding this! Making a deviation here - Wonderland_War
+		
+		CBasePlayer *pPlayer = GetPlayerHoldingEntity( this );
+		if (pPlayer)
+			pPlayer->ForceDropOfCarriedPhysObjects( this );
+		
+		ResetSequence( LookupSequence( "shortcircuit" ) );
+		EmitSound( "DoSparkSmaller" );
+		m_bIsABox = true;
+		m_bIsShortcircuit = true;
+	}
+}
+
+void CPropMonsterBox::OnPhysGunDrop( CBasePlayer *pPhysGunUser, PhysGunDrop_t reason )
+{
+	m_bHeld = false;
+	BaseClass::OnPhysGunDrop( pPhysGunUser, reason );
+	if ( !m_bIsShortcircuit )
+	{
+		if (m_nDeferredTransform)
+		{
+			if (m_nDeferredTransform == 1)
+			{
+				BecomeBox( true );
+				m_nDeferredTransform = 0;
+			}
+			else
+			{
+				if (m_nDeferredTransform == 2)
+					BecomeMonster( true );
+				m_nDeferredTransform = 0;
+			}
+		}
+		if (!m_bForcedAsBox)
+		{
+			ResetSequence( LookupSequence("hermit_out") );
+		}
+	}
+}
+
+
+void CPropMonsterBox::HandleAnimEvent( animevent_t *pEvent )
+{
+	int event_type;
+
+	if ((pEvent->type & 0x400) != 0)
+		event_type = pEvent->_event_lowword;
+	else
+		event_type = pEvent->_event_highword;
+
+	if ( event_type != 1100 || m_bHeld )
+	{
+		BaseClass::HandleAnimEvent(pEvent);
+	}
+	else
+	{
+		if (m_pPhysicsObject)
+		{
+			Vector vecForward;
+			Vector vecUp;
+			GetVectors( &vecForward, 0, &vecUp );
+			if (vecUp.z >= 0.1)
+			{
+				Vector vecAbsEnd;
+				vecAbsEnd.x = GetAbsOrigin().x - ( vecUp.x * 32.0 );
+				vecAbsEnd.y = GetAbsOrigin().y - ( vecUp.y * 32.0 );		
+
+				trace_t tr;
+				UTIL_TraceLine( GetAbsOrigin(), vecAbsEnd, 0x200400Bu, this, COLLISION_GROUP_NONE, &tr);
+				if (tr.fraction != 1.0)
+				{
+					Vector angImpulse = RandomAngularImpulse( -0.5, 0.5 );
+
+					float flRandom = RandomFloat(-0.2, 0.2);
+
+					vecAbsEnd.z = RandomFloat(0.69999999, 1.0);
+
+					Vector vecVelocity;
+
+					//vecVelocity.x = vecForward.x + flRandom;
+					//vecVelocity.y = vecForward.y + flRandom;
+					//vecVelocity.z = vecForward.z + vecAbsEnd.z;
+
+					float flVel = sv_monster_turret_velocity.GetFloat();
+					
+					vecVelocity.x = ( (vecForward.x + flRandom) * flVel ) * m_flPushStrength;
+					vecVelocity.y = ( (vecForward.y + flRandom) * flVel ) * m_flPushStrength;
+					vecVelocity.z = ( (vecForward.z + vecAbsEnd.z) * flVel ) * m_flPushStrength;
+					SetGroundEntity( NULL );
+					m_pPhysicsObject->Wake();
+
+					m_pPhysicsObject->SetVelocityInstantaneous( &vec3_origin, &angImpulse);
+					m_pPhysicsObject->ApplyForceCenter( vecVelocity );
+					ApplyAbsVelocityImpulse( vecVelocity );
+				}
+			}
+		}
+	}
+}
+
+void CPropMonsterBox::AnimateThink( void )
+{
+	StudioFrameAdvance();
+	DispatchAnimEvents( this );
+
+	SetContextThink( &CPropMonsterBox::AnimateThink, gpGlobals->curtime + 0.1, s_pAnimateThinkContext );
+
+	if ( m_bIsShortcircuit )
+	{
+		if ( m_bSequenceFinished )
+		{
+			CBaseAnimating::ResetSequence( LookupSequence( "shortcircuit" ) );
+		}
+	}
+	else
+	{
+		Vector vecVelocity = vec3_origin;
+		if (m_pPhysicsObject)
+			m_pPhysicsObject->GetVelocity( &vecVelocity, NULL );
+
+		if ((((vecVelocity.y * vecVelocity.y) + (vecVelocity.x * vecVelocity.x))
+			+ (vecVelocity.z * vecVelocity.z)) <= (m_flBoxSwitchSpeed * m_flBoxSwitchSpeed))
+		{
+			if (!m_bHeld && m_bIsFlying && !m_bForcedAsBox)
+			{
+				Vector v23 = CollisionProp()->OBBMins();
+				Vector v24 = CollisionProp()->OBBMaxs();
+
+				trace_t tr;
+
+				UTIL_TraceHull( GetAbsOrigin(), GetAbsOrigin(), v23, v24, 0x200400Bu, this, 0, &tr);
+				if (tr.startsolid || tr.allsolid)
+				{
+					Vector vecImpulse;
+					Vector vecUp;
+					GetVectors( 0, 0, &vecUp );
+					Vector angImpulse = RandomAngularImpulse( -0.5, 0.5);
+
+					//vecImpulse.x = vecUp.x + v32;
+					//vecImpulse.y = vecUp.y + v32;
+					//vecImpulse.z = vecUp.z + RandomFloat( 1.7, 2.0 );
+
+					vecImpulse.x = (vecUp.x + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
+					vecImpulse.y = (vecUp.y + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
+					vecImpulse.z = (vecUp.z + RandomFloat( 1.7, 2.0 ) ) * sv_monster_turret_velocity.GetFloat();
+					
+					SetGroundEntity( NULL );
+					
+					m_pPhysicsObject->Wake();
+					m_pPhysicsObject->SetVelocityInstantaneous( &vec3_origin, &angImpulse );
+					m_pPhysicsObject->ApplyForceCenter( vecImpulse );
+					
+					ApplyAbsVelocityImpulse( vecImpulse );
+				}
+				else
+				{
+					m_bIsFlying = true;
+					BecomeMonster( false );
+				}
+			}
+		}
+		else if (!m_bHeld && !m_bIsFlying)
+		{
+			m_bIsFlying = true;
+			BecomeBox( false );
+		}
+		if (m_bSequenceFinished)
+		{
+			if (m_bHeld || m_bForcedAsBox)
+			{
+				ResetSequence( LookupSequence("hermit_idle") );
+			}
+			else
+			{
+				Vector vecUp;
+				GetVectors(0, 0, &vecUp);
+				if (vecUp.z >= 0.60000002)
+				{
+
+					Vector vecImpulse = GetAbsOrigin() - ( vecUp * 32.0 );
+					trace_t tr;
+					UTIL_TraceLine( GetAbsOrigin(), vecImpulse, 0x200400Bu, this, 0, &tr );
+					if (tr.fraction == 1.0)
+					{
+						ResetSequence( LookupSequence("intheair") );
+					}
+					else
+					{
+						int iRandom1 = RandomInt( 0, 2 );
+						if (iRandom1)
+						{
+
+							int iRandom2 = iRandom1 - 1;
+							if (iRandom2)
+							{
+								if (iRandom2 == 1)
+								{
+									CBaseAnimating::ResetSequence( LookupSequence("straight03") );
+									m_flPushStrength = 1.1;
+								}
+							}
+							else
+							{
+								CBaseAnimating::ResetSequence( LookupSequence("straight02") );
+								m_flPushStrength = 0.69999999;
+							}
+						}
+						else
+						{
+							CBaseAnimating::ResetSequence( LookupSequence("straight01") );
+							m_flPushStrength = 1.0;
+						}
+					}
+				}
+				else
+				{
+					CBaseAnimating::ResetSequence( LookupSequence("fallover_idle") );
+				}
+			}
+		}
+	}
+}
+
+void CPropMonsterBox::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
+{
+	// Wtf? Linux has this function but windows doesn't, use BaseClass code for now.
+#if 0
+	if (pActivator)
+	{
+		if ((*((unsigned __int8(__cdecl **)(CBaseEntity *))pActivator->_vptr_IHandleEntity + 86))(pActivator))
+			(*((void(__cdecl **)(CBaseEntity *, CPropMonsterBox *const, int))pActivator->_vptr_IHandleEntity + 443))(
+			pActivator,
+			this,
+			1);
+	}
+#else
+	BaseClass::Use( pActivator, pCaller, useType, value );
+#endif
+}
+
+void CPropMonsterBox::OnPhysGunPickup( CBasePlayer *pPhysGunUser, PhysGunPickup_t reason )
+{
+	if ( !m_bHeld )
+	{
+		m_bHeld = true;
+		if (!m_bIsShortcircuit)
+		{
+			ResetSequence( LookupSequence( "hermit_in" ) );
+		}
+		BaseClass::OnPhysGunPickup( pPhysGunUser, reason );
+	}
+}
+
+int CPropMonsterBox::ObjectCaps( void )
+{
+	return BaseClass::ObjectCaps();
+}
+
+void CPropMonsterBox::OnFizzled( void )
+{
+	m_OnFizzled.FireOutput( this, this );
+}
+
+void CPropMonsterBox::InputBecomeBox( inputdata_t &inputdata )
+{
+	BecomeBox( true );
+}
+
+void CPropMonsterBox::InputBecomeMonster( inputdata_t &inputdata )
+{
+	BecomeMonster( true );
+}
+
+void CPropMonsterBox::InputBecomeShortcircuit( inputdata_t &inputdata )
+{
+	BecomeShortcircuit();
+}
+
+void CPropMonsterBox::InputDissolve( inputdata_t &inputdata )
+{
+	CTriggerPortalCleanser::FizzleBaseAnimating( NULL, this );
+}
+
+void CPropMonsterBox::InputSilentDissolve( inputdata_t &inputdata )
+{
+	if ( m_bAllowSilentDissolve )
+	{
+		OnFizzled();
+		UTIL_Remove(this);
+	}
 }
