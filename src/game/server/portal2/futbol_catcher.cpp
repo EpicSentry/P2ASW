08d8960a4d1e10ccdfe39dbf9e6910807384d609
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2023-10-04 13:43:14 +1300

    Update futbol_catcher.cpp

--- src/game/server/portal2/futbol_catcher.cpp
+++ src/game/server/portal2/futbol_catcher.cpp
@@ -1,51 +1,6 @@
 #include "cbase.h"
-#include "trace.h"
-#include "prop_glass_futbol.h"
+#include "futbol_catcher.h"
 #include "tier0/memdbgon.h"
-
-class CFutbolCatcher : public CBaseAnimating
-{
-public:
-	DECLARE_CLASS(CFutbolCatcher, CBaseAnimating);
-	DECLARE_DATADESC();
-
-	CFutbolCatcher::CFutbolCatcher()
-	{
-		m_vCatcherBoxHalfDiagonal.x = 25.0f;
-		m_vCatcherBoxHalfDiagonal.y = 25.0f;
-		m_vCatcherBoxHalfDiagonal.z = 25.0f;
-		m_vecCatchBoxMins = vec3_origin;
-		m_vecCatchBoxMaxs = vec3_origin;
-		m_vecCatchBoxOrig = vec3_origin;
-		m_bDisableRecaptureOnPlayerGrab = false;
-	}
-	void Spawn();
-
-protected:
-	CHandle<CBaseEntity> m_hCaughtFutbol;
-	Vector m_vCatcherBoxHalfDiagonal;
-	Vector m_vecCatchBoxMins;
-	Vector m_vecCatchBoxMaxs;
-	Vector m_vecCatchBoxOrig;
-	bool m_bDisableRecaptureOnPlayerGrab;
-	COutputEvent m_OnFutbolReleased;
-	COutputEvent m_OnFutbolCaught;
-	void CatchThink();
-	void CaptureThink();
-	void CaptureFutbol(CPropGlassFutbol *pFutbol);
-};
-
-class CFilterOnlyGlassFutbol : public CTraceFilterSimple
-{
-	DECLARE_CLASS(CFilterOnlyGlassFutbol, CTraceFilterSimple);
-public:
-	CFilterOnlyGlassFutbol(const IHandleEntity* passentity, int collisionGroup)
-		: CTraceFilterSimple(passentity, collisionGroup)
-	{
-	}
-
-	virtual bool ShouldHitEntity(IHandleEntity* pHandleEntity, int contentsMask);
-};
 
 LINK_ENTITY_TO_CLASS(futbol_catcher, CFutbolCatcher);
 
