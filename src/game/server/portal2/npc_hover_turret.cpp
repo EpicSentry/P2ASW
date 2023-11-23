4ae4373ec4ad3d59f76dd85c57fc5f5b681bc2b6
Jordon87 <38020644+Jordon87@users.noreply.github.com>
2023-11-24 10:20:53 +1300

    Update npc_hover_turret.cpp

--- src/game/server/portal2/npc_hover_turret.cpp
+++ src/game/server/portal2/npc_hover_turret.cpp
@@ -4,6 +4,8 @@
 #include "smoke_trail.h"
 #include "tier0/memdbgon.h"
 
+extern ConVar sv_gravity;
+
 enum hoverTurretState_e
 {
 	HOVER_TURRET_TARGETING = 0,
@@ -52,16 +54,21 @@ int ACT_HOVER_TURRET_DISABLED;
 class CNPC_HoverTurret : public CNPCBaseInteractive<CAI_BasePhysicsFlyingBot>, public CDefaultPlayerPickupVPhysics
 {
 public:
-	DECLARE_CLASS(CNPC_HoverTurret, CNPCBaseInteractive<CAI_BasePhysicsFlyingBot>);
 	//DECLARE_SERVERCLASS();
+	DECLARE_CLASS(CNPC_HoverTurret, CNPCBaseInteractive<CAI_BasePhysicsFlyingBot>);
 	DECLARE_DATADESC();
 	DEFINE_CUSTOM_AI;
 
 	void Event_Killed(const CTakeDamageInfo& info);
 	void TraceAttack(const CTakeDamageInfo& info, const Vector& vecDir, trace_t* ptr) { return BaseClass::TraceAttack(info, vecDir, ptr); }
-	
 	void UpdateOnRemove();
 	void MoveToTarget(float flInterval, const Vector& vMoveTarget);
+	void MoveExecute_Alive(float flInterval);
+	Vector BodyTarget(const Vector& posSrc, bool bNoisy);
+	float GetHeadTurnRate() { return 45.0f; }
+	void TurnHeadToTarget(float flInterval, const Vector& MoveTarget);
+	bool UpdateFacing();
+	void UpdateMuzzleMatrix();
 	void CreateSmokeTrail();
 	void Precache();
 	void RunTask(const Task_t* pTask);
@@ -85,8 +92,11 @@ private:
 	float m_flEngineStallTime;
 
 	hoverTurretAttackState_e m_iFiringState;
+	
+	matrix3x4_t m_muzzleToWorld;
 
 	int m_iMuzzleAttachment;
+	int m_muzzleToWorldTick;
 
 	QAngle m_vecGoalAngles;
 	QAngle m_vInitialLookAngles;
@@ -139,6 +149,94 @@ void CNPC_HoverTurret::MoveToTarget(float flInterval, const Vector& vMoveTarget)
 	if (flInterval > 0.0f && m_flEngineStallTime <= gpGlobals->curtime && m_iFiringState != HOVER_TURRET_SHOT_DISABLED)
 	{
 
+	}
+}
+
+void CNPC_HoverTurret::MoveExecute_Alive(float flInterval)
+{
+	BaseClass::PhysicsCheckWaterTransition();
+
+	if (m_pPhysicsObject)
+	{
+		if (m_hTether)
+		{
+			m_pPhysicsObject->Wake();
+		}
+	}
+
+	m_vCurrentVelocity.x = m_vForceVelocity.x;
+	m_vCurrentVelocity.y = m_vForceVelocity.y;
+	m_vCurrentVelocity.z = m_vForceVelocity.z - (flInterval * 0.1) * sv_gravity.GetFloat();
+	m_vForceVelocity = vec3_origin;
+}
+
+Vector CNPC_HoverTurret::BodyTarget(const Vector& posSrc, bool bNoisy)
+{
+	return WorldSpaceCenter();
+}
+
+void CNPC_HoverTurret::TurnHeadToTarget(float flInterval, const Vector& MoveTarget)
+{
+	Vector vecDir;
+	vecDir = MoveTarget - GetLocalOrigin();
+
+	float newYaw = BaseClass::VecToYaw(vecDir);
+
+	float time = gpGlobals->curtime - GetLastThink();
+	float yawSpeedPerSec = GetHeadTurnRate() * 10.0f;
+	
+	float clampYaw = AI_ClampYaw(yawSpeedPerSec, m_fHeadYaw, newYaw, time);
+	
+	if (clampYaw != m_fHeadYaw)
+		m_fHeadYaw = clampYaw;
+}
+
+bool CNPC_HoverTurret::UpdateFacing()
+{
+	bool  bMoved = false;
+	UpdateMuzzleMatrix();
+
+	Vector vecGoalDir;
+	AngleVectors(m_vecGoalAngles, &vecGoalDir);
+
+	Vector vecGoalLocalDir;
+	VectorIRotate(vecGoalDir, m_muzzleToWorld, vecGoalLocalDir);
+
+	QAngle vecGoalLocalAngles;
+	VectorAngles(vecGoalLocalDir, vecGoalLocalAngles);
+
+	// Update pitch
+	float flDiff = AngleNormalize(UTIL_ApproachAngle(vecGoalLocalAngles.x, 0.0, 0.05f * MaxYawSpeed()));
+
+	SetPoseParameter(m_poseAim_Pitch, GetPoseParameter(m_poseAim_Pitch) + (flDiff / 2.5f));
+
+	if (fabs(flDiff) > 0.1f)
+	{
+		bMoved = true;
+	}
+
+	// Update yaw
+	flDiff = AngleNormalize(UTIL_ApproachAngle(vecGoalLocalAngles.y, 0.0, 0.05f * MaxYawSpeed()));
+
+	SetPoseParameter(m_poseAim_Yaw, GetPoseParameter(m_poseAim_Yaw) + (flDiff / 2.5f));
+
+	if (fabs(flDiff) > 0.1f)
+	{
+		bMoved = true;
+	}
+
+	// You're going to make decisions based on this info.  So bump the bone cache after you calculate everything
+	InvalidateBoneCache();
+
+	return bMoved;
+}
+
+void CNPC_HoverTurret::UpdateMuzzleMatrix()
+{
+	if (gpGlobals->tickcount != m_muzzleToWorldTick)
+	{
+		m_muzzleToWorldTick = gpGlobals->tickcount;
+		GetAttachment(m_iMuzzleAttachment, m_muzzleToWorld);
 	}
 }
 
