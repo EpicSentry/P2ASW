f9cc3fe2b935f0e88128b2090b970323d8c996d9
Mandrew <Wonderland_War@outlook.com>
2024-07-23 02:38:47 -0500

    Update prop_monster_box.cpp
    This won't fix all the problems but this is more accurate.

--- src/game/server/portal2/prop_monster_box.cpp
+++ src/game/server/portal2/prop_monster_box.cpp
@@ -3,7 +3,7 @@
 #include "portal_grabcontroller_shared.h"
 #include "trigger_portal_cleanser.h"
 
-const char *s_pAnimateThinkContext = "AnimateThinkContext";
+const char *s_pAnimateThinkContext = "Animate";
 
 
 ConVar sv_monster_turret_velocity( "sv_monster_turret_velocity", "100.0f", FCVAR_REPLICATED, "The amount of velocity the monster turret tries to move with." );
@@ -41,7 +41,12 @@ LINK_ENTITY_TO_CLASS( prop_monster_box, CPropMonsterBox )
 
 CPropMonsterBox::CPropMonsterBox()
 {
-
+	m_nDeferredTransform = 0;
+	m_flPushStrength = 1.0;
+	m_flBoxSwitchSpeed = 400.0;
+	m_bIsShortcircuit = false;
+	m_bHeld = false;
+	m_bAllowSilentDissolve = false;
 }
 
 CPropMonsterBox::~CPropMonsterBox()
@@ -57,46 +62,45 @@ void CPropMonsterBox::Spawn( void )
 	if (!m_bForcedAsBox)
 		pszModelName = "models/npcs/monsters/monster_a.mdl";
 	
-	SetModel(pszModelName);
+	SetModel( pszModelName );
 	
 	CollisionProp()->SetSolid( SOLID_BBOX );
-	AddEffects( 1024 );
+	AddEffects( EF_NOFLASHLIGHT );
 	//g_PortalGameStats.Event_CubeSpawn();
 	BaseClass::Spawn();
 	if ( m_bForcedAsBox )
 	{
 		ResetSequence( LookupSequence("hermit_idle") );
-
+		SetCycle( 0.0 );
 		m_bIsABox = true;
 	}
 	else
 	{
 		ResetSequence( LookupSequence("straight01") );
-		
+		SetCycle( 0.0 );
 		m_bIsABox = false;
 	}
 
-	m_nBodyGroups = RandomInt( 0, 15);
-	m_nBodyGroups |= RandomInt( 0, 15);
-	m_nBodyGroups |= RandomInt( 0, 15);
+	m_nBodyGroups = RandomInt( 0, 15 );
+	m_nBodyGroups |= RandomInt( 0, 15 );
+	m_nBodyGroups |= RandomInt( 0, 15 );
 	
 
-	SetBodygroup(1, m_nBodyGroups & 1);
-	SetBodygroup(2, (m_nBodyGroups >> 1) & 1);
-	SetBodygroup(3, (m_nBodyGroups >> 2) & 1);
-	SetBodygroup(4, (m_nBodyGroups >> 3) & 1);
+	SetBodygroup( 1, m_nBodyGroups & 1 );
+	SetBodygroup( 2, ((m_nBodyGroups >> 1) & 1) != 0 );
+	SetBodygroup( 3, ((m_nBodyGroups >> 2) & 1) != 0 );
+	SetBodygroup( 4, ((m_nBodyGroups >> 3) & 1) != 0 );
 	
-	SetMoveType( MOVETYPE_VPHYSICS, MOVECOLLIDE_DEFAULT );
-
-
+	SetMoveType( MOVETYPE_VPHYSICS );
+	
 	SetContextThink( &CPropMonsterBox::AnimateThink, gpGlobals->curtime + 0.1, s_pAnimateThinkContext );
 
-	if (VPhysicsGetObject())
-		VPhysicsGetObject()->SetMass(40.0);
+	if ( VPhysicsGetObject() )
+		VPhysicsGetObject()->SetMass( 40.0 );
 	
 	UpdateObjectCapsCache();
-	SetFadeDistance(-1.0, 0.0);
-	SetGlobalFadeScale(0.0);
+	SetFadeDistance( -1.0, 0.0 );
+	SetGlobalFadeScale( 0.0 );
 }
 
 void CPropMonsterBox::Precache( void )
@@ -146,29 +150,29 @@ void CPropMonsterBox::BecomeMonster( bool bForce )
 {
 	if ( m_bIsShortcircuit )
 		return;
-
-	Vector vecVelocity; // [esp+4h] [ebp-18h] BYREF
-	Vector vecAngVelocity; // [esp+10h] [ebp-Ch] BYREF
-
+	
 	if ( bForce )
 	{
 		m_bForcedAsBox = false;
 	}
-	else if (m_bForcedAsBox)
+	else if ( m_bForcedAsBox )
 	{
 		return;
 	}
-	if (m_bIsABox)
+
+	if ( m_bIsABox )
 	{
-		if (m_bHeld)
+		if ( m_bHeld )
 		{
 			m_nDeferredTransform = 2;
 		}
 		else
 		{
 			m_bIsABox = false;
-			if (VPhysicsGetObject())
+			if ( VPhysicsGetObject() )
 			{
+				Vector vecVelocity;
+				Vector vecAngVelocity;
 				VPhysicsGetObject()->GetVelocity( &vecVelocity, &vecAngVelocity );
 				
 				SetModel("models/npcs/monsters/monster_a.mdl");
@@ -189,9 +193,9 @@ void CPropMonsterBox::BecomeMonster( bool bForce )
 
 void CPropMonsterBox::BecomeShortcircuit( void )
 {
-	if (VPhysicsGetObject())
+	if ( VPhysicsGetObject() )
 	{
-		if (!m_bIsABox)
+		if ( !m_bIsABox )
 		{
 			Vector vecVelocity;
 			Vector vecAngVelocity;
@@ -207,7 +211,7 @@ void CPropMonsterBox::BecomeShortcircuit( void )
 		// to drop what they're grabbing even if they aren't holding this! Making a deviation here - Wonderland_War
 		
 		CBasePlayer *pPlayer = GetPlayerHoldingEntity( this );
-		if (pPlayer)
+		if ( pPlayer )
 			pPlayer->ForceDropOfCarriedPhysObjects( this );
 		
 		ResetSequence( LookupSequence( "shortcircuit" ) );
@@ -223,23 +227,21 @@ void CPropMonsterBox::OnPhysGunDrop( CBasePlayer *pPhysGunUser, PhysGunDrop_t re
 	BaseClass::OnPhysGunDrop( pPhysGunUser, reason );
 	if ( !m_bIsShortcircuit )
 	{
-		if (m_nDeferredTransform)
+		if ( m_nDeferredTransform )
 		{
-			if (m_nDeferredTransform == 1)
+			if ( m_nDeferredTransform == 1 )
 			{
 				BecomeBox( true );
-				m_nDeferredTransform = 0;
 			}
-			else
-			{
-				if (m_nDeferredTransform == 2)
-					BecomeMonster( true );
-				m_nDeferredTransform = 0;
+			else if ( m_nDeferredTransform == 2 )
+			{				
+				BecomeMonster( true );
 			}
+			m_nDeferredTransform = 0;
 		}
-		if (!m_bForcedAsBox)
+		if ( !m_bForcedAsBox )
 		{
-			ResetSequence( LookupSequence("hermit_out") );
+			ResetSequence( LookupSequence( "hermit_out" ) );
 		}
 	}
 }
@@ -249,7 +251,7 @@ void CPropMonsterBox::HandleAnimEvent( animevent_t *pEvent )
 {
 	int event_type;
 
-	if ((pEvent->type & 0x400) != 0)
+	if ((pEvent->type & AE_TYPE_NEWEVENTSYSTEM) != 0)
 		event_type = pEvent->_event_lowword;
 	else
 		event_type = pEvent->_event_highword;
@@ -260,26 +262,24 @@ void CPropMonsterBox::HandleAnimEvent( animevent_t *pEvent )
 	}
 	else
 	{
-		if (VPhysicsGetObject())
+		if ( VPhysicsGetObject() )
 		{
 			Vector vecForward;
 			Vector vecUp;
-			GetVectors( &vecForward, 0, &vecUp );
-			if (vecUp.z >= 0.1)
+			GetVectors( &vecForward, NULL, &vecUp );
+			if ( vecUp.z >= 0.1 )
 			{
 				Vector vecAbsEnd;
 				vecAbsEnd.x = GetAbsOrigin().x - ( vecUp.x * 32.0 );
 				vecAbsEnd.y = GetAbsOrigin().y - ( vecUp.y * 32.0 );		
 
 				trace_t tr;
-				UTIL_TraceLine( GetAbsOrigin(), vecAbsEnd, 0x200400Bu, this, COLLISION_GROUP_NONE, &tr);
+				UTIL_TraceLine( GetAbsOrigin(), vecAbsEnd, MASK_SOLID, this, COLLISION_GROUP_NONE, &tr);
 				if (tr.fraction != 1.0)
 				{
 					Vector angImpulse = RandomAngularImpulse( -0.5, 0.5 );
-
-					float flRandom = RandomFloat(-0.2, 0.2);
-
-					vecAbsEnd.z = RandomFloat(0.69999999, 1.0);
+					
+					vecAbsEnd.z = RandomFloat(0.7, 1.0);
 
 					Vector vecVelocity;
 
@@ -289,8 +289,8 @@ void CPropMonsterBox::HandleAnimEvent( animevent_t *pEvent )
 
 					float flVel = sv_monster_turret_velocity.GetFloat();
 					
-					vecVelocity.x = ( (vecForward.x + flRandom) * flVel ) * m_flPushStrength;
-					vecVelocity.y = ( (vecForward.y + flRandom) * flVel ) * m_flPushStrength;
+					vecVelocity.x = ( (vecForward.x + RandomFloat(-0.2, 0.2)) * flVel ) * m_flPushStrength;
+					vecVelocity.y = ( (vecForward.y + RandomFloat(-0.2, 0.2)) * flVel ) * m_flPushStrength;
 					vecVelocity.z = ( (vecForward.z + vecAbsEnd.z) * flVel ) * m_flPushStrength;
 					SetGroundEntity( NULL );
 					VPhysicsGetObject()->Wake();
@@ -317,111 +317,104 @@ void CPropMonsterBox::AnimateThink( void )
 		{
 			CBaseAnimating::ResetSequence( LookupSequence( "shortcircuit" ) );
 		}
+
+		return;
 	}
-	else
+
+	Vector vecVelocity = vec3_origin;
+	if (VPhysicsGetObject())
+		VPhysicsGetObject()->GetVelocity( &vecVelocity, NULL );
+
+	if ( vecVelocity.LengthSqr() <= Sqr( m_flBoxSwitchSpeed ) )
 	{
-		Vector vecVelocity = vec3_origin;
-		if (VPhysicsGetObject())
-			VPhysicsGetObject()->GetVelocity( &vecVelocity, NULL );
-
-		if ((((vecVelocity.y * vecVelocity.y) + (vecVelocity.x * vecVelocity.x))
-			+ (vecVelocity.z * vecVelocity.z)) <= (m_flBoxSwitchSpeed * m_flBoxSwitchSpeed))
+		if ( !m_bHeld && m_bIsFlying && !m_bForcedAsBox )
 		{
-			if (!m_bHeld && m_bIsFlying && !m_bForcedAsBox)
+			trace_t tr;
+			UTIL_TraceHull( GetAbsOrigin(), GetAbsOrigin(), CollisionProp()->OBBMins(), CollisionProp()->OBBMaxs(), MASK_SOLID, this, COLLISION_GROUP_NONE, &tr );
+			if ( tr.startsolid || tr.allsolid )
 			{
-				Vector v23 = CollisionProp()->OBBMins();
-				Vector v24 = CollisionProp()->OBBMaxs();
+				Vector vecUp;
+				GetVectors( NULL, NULL, &vecUp );
+				Vector angImpulse = RandomAngularImpulse( -0.5, 0.5 );
 
-				trace_t tr;
+				//vecImpulse.x = vecUp.x + v32;
+				//vecImpulse.y = vecUp.y + v32;
+				//vecImpulse.z = vecUp.z + RandomFloat( 1.7, 2.0 );
 
-				UTIL_TraceHull( GetAbsOrigin(), GetAbsOrigin(), v23, v24, 0x200400Bu, this, 0, &tr);
-				if (tr.startsolid || tr.allsolid)
-				{
-					Vector vecImpulse;
-					Vector vecUp;
-					GetVectors( 0, 0, &vecUp );
-					Vector angImpulse = RandomAngularImpulse( -0.5, 0.5);
-
-					//vecImpulse.x = vecUp.x + v32;
-					//vecImpulse.y = vecUp.y + v32;
-					//vecImpulse.z = vecUp.z + RandomFloat( 1.7, 2.0 );
-
-					vecImpulse.x = (vecUp.x + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
-					vecImpulse.y = (vecUp.y + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
-					vecImpulse.z = (vecUp.z + RandomFloat( 1.7, 2.0 ) ) * sv_monster_turret_velocity.GetFloat();
+				Vector vecImpulse;
+				vecImpulse.x = (vecUp.x + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
+				vecImpulse.y = (vecUp.y + RandomFloat( -0.2, 0.2 ) ) * sv_monster_turret_velocity.GetFloat();
+				vecImpulse.z = (vecUp.z + RandomFloat( 1.7, 2.0 ) ) * sv_monster_turret_velocity.GetFloat();
 					
-					SetGroundEntity( NULL );
+				SetGroundEntity( NULL );
 					
-					VPhysicsGetObject()->Wake();
-					VPhysicsGetObject()->SetVelocityInstantaneous( &vec3_origin, &angImpulse );
-					VPhysicsGetObject()->ApplyForceCenter( vecImpulse );
+				VPhysicsGetObject()->Wake();
+				VPhysicsGetObject()->SetVelocityInstantaneous( &vec3_origin, &angImpulse );
+				VPhysicsGetObject()->ApplyForceCenter( vecImpulse );
 					
-					ApplyAbsVelocityImpulse( vecImpulse );
-				}
-				else
-				{
-					m_bIsFlying = true;
-					BecomeMonster( false );
-				}
-			}
-		}
-		else if (!m_bHeld && !m_bIsFlying)
-		{
-			m_bIsFlying = true;
-			BecomeBox( false );
-		}
-		if (m_bSequenceFinished)
-		{
-			if (m_bHeld || m_bForcedAsBox)
-			{
-				ResetSequence( LookupSequence("hermit_idle") );
+				ApplyAbsVelocityImpulse( vecImpulse );
 			}
 			else
 			{
-				Vector vecUp;
-				GetVectors(0, 0, &vecUp);
-				if (vecUp.z >= 0.60000002)
+				m_bIsFlying = true;
+				BecomeMonster( false );
+			}
+		}
+	}
+	else if ( !m_bHeld && !m_bIsFlying )
+	{
+		m_bIsFlying = true;
+		BecomeBox( false );
+	}
+	if ( m_bSequenceFinished )
+	{
+		if ( m_bHeld || m_bForcedAsBox )
+		{
+			ResetSequence( LookupSequence( "hermit_idle" ) );
+		}
+		else
+		{
+			Vector vecUp;
+			GetVectors( NULL, NULL, &vecUp );
+			if ( vecUp.z >= 0.6 )
+			{
+				Vector vecImpulse = GetAbsOrigin() - ( vecUp * 32.0 );
+				trace_t tr;
+				UTIL_TraceLine( GetAbsOrigin(), vecImpulse, MASK_SOLID, this, COLLISION_GROUP_NONE, &tr );
+				if ( tr.fraction == 1.0 )
 				{
-
-					Vector vecImpulse = GetAbsOrigin() - ( vecUp * 32.0 );
-					trace_t tr;
-					UTIL_TraceLine( GetAbsOrigin(), vecImpulse, 0x200400Bu, this, 0, &tr );
-					if (tr.fraction == 1.0)
+					ResetSequence( LookupSequence( "intheair" ) );
+				}
+				else
+				{
+					int iRandom1 = RandomInt( 0, 2 );
+					if ( iRandom1 )
 					{
-						ResetSequence( LookupSequence("intheair") );
-					}
-					else
-					{
-						int iRandom1 = RandomInt( 0, 2 );
-						if (iRandom1)
+						int iRandom2 = iRandom1 - 1;
+						if ( iRandom2 )
 						{
-
-							int iRandom2 = iRandom1 - 1;
-							if (iRandom2)
+							if ( iRandom2 == 1 )
 							{
-								if (iRandom2 == 1)
-								{
-									CBaseAnimating::ResetSequence( LookupSequence("straight03") );
-									m_flPushStrength = 1.1;
-								}
-							}
-							else
-							{
-								CBaseAnimating::ResetSequence( LookupSequence("straight02") );
-								m_flPushStrength = 0.69999999;
+								CBaseAnimating::ResetSequence( LookupSequence( "straight03" ) );
+								m_flPushStrength = 1.1;
 							}
 						}
 						else
 						{
-							CBaseAnimating::ResetSequence( LookupSequence("straight01") );
-							m_flPushStrength = 1.0;
+							CBaseAnimating::ResetSequence( LookupSequence( "straight02" ) );
+							m_flPushStrength = 0.7;
 						}
 					}
+					else
+					{
+						CBaseAnimating::ResetSequence( LookupSequence( "straight01" ) );
+						m_flPushStrength = 1.0;
+					}
 				}
-				else
-				{
-					CBaseAnimating::ResetSequence( LookupSequence("fallover_idle") );
-				}
+			}
+			else
+			{
+				CBaseAnimating::ResetSequence( LookupSequence( "fallover_idle" ) );
 			}
 		}
 	}
@@ -429,7 +422,7 @@ void CPropMonsterBox::AnimateThink( void )
 
 void CPropMonsterBox::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
 {
-	// Wtf? Linux has this function but windows doesn't, use BaseClass code for now.
+	// Linux has this function but windows doesn't, use BaseClass code for now.
 #if 0
 	if (pActivator)
 	{
@@ -459,7 +452,7 @@ void CPropMonsterBox::OnPhysGunPickup( CBasePlayer *pPhysGunUser, PhysGunPickup_
 
 int CPropMonsterBox::ObjectCaps( void )
 {
-	return BaseClass::ObjectCaps();
+	return BaseClass::ObjectCaps() | FCAP_IMPULSE_USE | FCAP_USE_IN_RADIUS;
 }
 
 void CPropMonsterBox::OnFizzled( void )
